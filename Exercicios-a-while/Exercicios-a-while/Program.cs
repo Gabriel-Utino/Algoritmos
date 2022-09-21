2f654028542127d1e368fed9fcf97506b329775f
Gabriel-Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2022-09-21 10:58:34 +0900

    test
    exericicio

---
+++ Exercicios-a-while/Exercicios-a-while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_a_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercícios :Apresentar todos os valores numéricos inteiros impares situados nafaixa de 0 a 20.
+            int i = 1;
+            while (i <= 20)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+        }
+    }
+}
