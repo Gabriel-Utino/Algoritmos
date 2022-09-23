afb6a7d836167dbbf72545985e692d47a8cf9c3e
Gabriel-Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2022-09-24 06:21:29 +0900

    課題
    課題一つ目

---
+++ Aticidades/2a Atividade/Atividade1/Atividade1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Dados: 10 números digitados pelo usuário. Construa algoritmo para mostrar os
+            valores negativos e que calcule e mostre a média dos valores menores que zero.*/
+            int i = 0;
+            double average = 0;
+            double[] array = new double[10];
+            var negative = new List<double>();
+            while (i < 10)
+            {
+                Console.Write("degite numero :");
+                array[i] = double.Parse(Console.ReadLine());
+                if (array[i] < 0)
+                {
+                    negative.Add(array[i]);
+                }
+                average = average + array[i];
+                i++;
+            }
+            average = average / 10;
+            Console.Write("valores negativos :");
+            foreach (var str in negative)
+            {
+                Console.Write(str);
+            }
+            Console.WriteLine("\nmedia dos valores :" + average);
+        }
+    }
+}
