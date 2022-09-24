4982cb7549bb7b91bbe8482e7ef596108d1905e9
Gabriel-Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2022-09-24 09:52:55 +0900

    課題
    期限10/3

---
+++ Aticidades/2a Atividade/Exercico2/Exercico2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercico2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0, totalprice;
+            double[] guest = new double[150];
+            while (i <= 3)
+            {
+                Console.WriteLine("");
+                totalprice = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
