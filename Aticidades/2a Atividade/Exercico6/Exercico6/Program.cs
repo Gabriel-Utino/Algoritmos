a3f38ac92c0aafef2fe2f365ca17faefe812d004
Gabriel-Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2022-09-27 19:09:49 +0900

    課題
    続き（4と5）

---
+++ Aticidades/2a Atividade/Exercico6/Exercico6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercico6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y;
+            int[] vetor = new int[8];
+            for(int i=0; i<8; i++)
+            {
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+        }
+    }
+}
+/*6. Faça um programa que leia um vetor de 8 posições e, em seguida, leia também dois
+valores X e Y quaisquer correspondentes a duas posições no vetor. Ao final seu
+programa deverá escrever a soma dos valores encontrados nas respectivas posições X e Y.*/
