21d0924a34d8c9c3b08506d2634233fb4d1c58c6
Gabriel-Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2022-10-12 05:03:48 +0900

    4a atividade

---
+++ Atividades/4a Atividade/exercicio3/exercicio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint[] arrayQ = new uint[10];
+            uint max;
+            int element;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("digite um numero :");
+                arrayQ[i] = uint.Parse(Console.ReadLine());
+            }
+            max = arrayQ.Max();
+            element = Array.IndexOf(arrayQ, max);
+
+            Console.WriteLine("maior valor :{0}", max);
+            Console.WriteLine("O element :{0}", element);
+
+        }
+    }
+}
---
+++ Atividades/4a Atividade/exercicio4/exercicio4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint[] arrayQ = new uint[10];
+            uint min;
+            int element;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("digite um numero :");
+                arrayQ[i] = uint.Parse(Console.ReadLine());
+            }
+            min = arrayQ.Min();
+            element = Array.IndexOf(arrayQ, min);
+            Console.WriteLine("menor valor :{0}", min);
+            Console.WriteLine("O element :{0}", element);
+
+        }
+    }
+}
