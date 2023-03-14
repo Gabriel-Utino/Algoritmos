e8b1f278de61a60d2eb396291b75421beb4bd663
Gabriel-Utino <110830104+Gabriel-Utino@users.noreply.github.com>
2023-03-15 03:35:31 +0900

    更新

---
+++ test/test/test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double base_maior, base_menor, altura, area;
+
+            Console.Write("escreva voler do base maior : ");
+            base_maior = double.Parse(Console.ReadLine());
+
+            Console.Write("escreva voler do base menor : ");
+            base_menor = double.Parse(Console.ReadLine());
+
+            Console.Write("escreva voler do altura : ");
+            altura = double.Parse(Console.ReadLine());
+
+            area = ((altura * base_maior) + (altura * base_menor)) / 2;
+            Console.WriteLine("area do Trapezios : " + area);
+
+        }
+    }
+}
---
+++ test/test/test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// 制御されます。アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("test")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定します。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("4a69b09e-2c12-4ad9-a49f-42d0c204dbce")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      リビジョン
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
